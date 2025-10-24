1abe79fab6868881ba6b444001b14b987e6329f1
sonali gandgalekar <sonaligandgalekar@gmail.com>
2025-10-24 10:16:27 +0530

    save to local

--- arithmetic_operation.py
+++ arithmetic_operation.py
@@ -1,7 +1,10 @@
-# Program: Addition and Subtraction of Two Numbers
-
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 print("Addition:", a + b)
 print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+if b != 0:
+    print("Division:", a / b)
+else:
+    print("Division: Not possible (division by zero)")
