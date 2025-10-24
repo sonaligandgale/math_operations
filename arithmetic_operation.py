153d7efc9d187564716cc11aa4295a46fa902be5
sonaligandgale <sonaligandgalekar@gmail.com>
2025-10-24 09:55:11 +0530

    Create arithmetic_operation.py

---
+++ arithmetic_operation.py
@@ -0,0 +1,7 @@
+# Program: Addition and Subtraction of Two Numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
